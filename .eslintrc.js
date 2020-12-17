425d24a5c68a17a5c0e14226508d1e8f48943689
Duncan Hamill <duncanrhamill@googlemail.com>
2020-12-17 14:43:17 +0000

    Added vuetify, set prettier to use 4 spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,16 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "indent": ["off"],
+    "prettier/prettier": [
+      "warn",
+      {
+        tabWidth: 4,
+        trailingComma: true,
+        singleQuote: true,
+        semi: true
+      }
+    ]
   }
 };
